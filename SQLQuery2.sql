cbce95b75bee8722d88b6f6839d72665044d7ee7
himanshu01031997 <hs010397@gmail.com>
2022-11-08 15:00:48 +0530

    add:multiple contact

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -16,3 +16,12 @@ email varchar(200)
 )
 
 select * from AddressBook
+-----------------UC3-----------------
+
+insert into AddressBook(firstname,lastname,address,city,state,zip,phoneno,email) values('hemu','sinha','bsp','pune','mh',456,33344,'dffdf')
+insert into AddressBook(firstname,lastname,address,city,state,zip,phoneno,email) values('himanshu','sinha','bhilai','pune','mh',456,33344,'dffdf')
+insert into AddressBook(firstname,lastname,address,city,state,zip,phoneno,email) values('rahul','sinha','bsp','pune','mh',456,33344,'dffdf')
+insert into AddressBook(firstname,lastname,address,city,state,zip,phoneno,email) values('virat','sinha','durg','pune','mh',456,33344,'dffdf')
+insert into AddressBook(firstname,lastname,address,city,state,zip,phoneno,email) values('harbhajan','sinha','bhind','pune','mh',456,33344,'dffdf')
+
+select * from AddressBook
