b7b1dfe10ca687c39e11a0048d2ba3e7392ee433
himanshu01031997 <hs010397@gmail.com>
2022-11-08 14:43:48 +0530

    add:table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,18 @@
+---------------UC1--------------
 create database AddressBookService
+
+
+---------------UC2---------------
+create table AddressBook
+(
+firstname varchar(200),
+lastname varchar(200),
+address varchar(200),
+city varchar(200),
+state varchar(200),
+zip int ,
+phoneno int,
+email varchar(200)
+)
+
+select * from AddressBook
